298f074434451ae4db23bd5620c3877ff921a745
Gustavo Luna <gustavolunalimon@gmail.com>
2021-05-14 18:29:27 -0500

    [mod] header funciton

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,12 +1,46 @@
-import React from "react";
+import React, {useRef, useEffect} from 'react'
+import {TweenMax} from 'gsap'
+import Hero from './sections/Hero'
+import Information from './sections/Information'
 
-class Main extends React.Component {
-    render() {
-        return (
-            <section>
-            </section>
-        )
-    }
+const Main = () => {
+
+    let body = useRef(null)
+
+    useEffect(() => {
+        // Get Hourly Day
+        let hourlyBackground = ''
+        let day = new Date();
+        let hora = day.getHours();
+        let minutos = day.getMinutes();
+
+        if( hora >= 7 && minutos >= 0 && hora <= 12 && minutos <= 59 ) {
+            // DIA
+            hourlyBackground = '#84B8D9';
+        }
+        if( hora >= 13 && minutos >= 0 && hora <= 18 && minutos <= 59 ) {
+            // TARDE
+            hourlyBackground = '#F76C01';
+        }
+        if( hora >= 19 && minutos >= 0 && hora <= 23 && minutos <= 59 ) {
+            // NOCHE
+            hourlyBackground = '#1C2D8C';
+        }
+        if(hora >= 0 && minutos >= 0 && hora <= 6 && minutos <= 59) {
+            // MADRUGADA
+            hourlyBackground = "#121212";
+        }
+
+        TweenMax.to(body, 0.8, {background: `${hourlyBackground}`})
+
+    })
+
+    return (
+        <main ref={ el => body = el}>
+            <Hero />
+            <Information />
+        </main>
+    )
 }
 
 export default Main
---
+++ src/components/sections/Information.js
@@ -0,0 +1,26 @@
+import React from 'react'
+
+const Information = () => {
+
+    return (
+        <section className="information">
+            <div className="container">
+                <div className="grid">
+                    <div className="col-lg-6">
+                        <div className="cont-info">
+                            <h2>Lorem ipsum dolor sit amet</h2>
+                            <p>Donec condimentum maximus lectus sed pellentesque. In imperdiet pellentesque enim ac cursus. Duis placerat sit amet dolor vel mollis. Mauris sagittis ultricies orci, id suscipit justo varius non. Vestibulum et arcu eu lorem posuere commodo. Aenean molestie dictum nisl a vehicula. Praesent hendrerit ac dolor in fermentum.</p>
+                        </div>
+                    </div>
+                    <div className="col-lg-6">
+                        <div className="multimedia">
+                            <img src="https://cdn.fastly.picmonkey.com/contentful/h6goo9gw1hh6/6NPSaM2xNL0vzz1mzz5Slt/efa79333c4fba9631cf4170c03693a39/1-intro-photo-final.jpg?w=800&q=70" alt="profile" />
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+    )
+}
+
+export default Information
